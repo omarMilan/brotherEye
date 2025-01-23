0b611e4c34f76b122c443a785614e45fff0f943e
omarMilan <131297774+omarMilan@users.noreply.github.com>
2025-01-23 14:08:07 -0600

    small update

--- brotherEye/src/heroPage/heroInfoTwo.jsx
+++ brotherEye/src/heroPage/heroInfoTwo.jsx
@@ -1,7 +1,7 @@
 import HeroDetails from "./heroDetails";
 export default function HeroInfoTwo({ name }) {
   return (
-    <div className="w-[307px] h-[213px] bg-containerColor hover:ring-2 hover:ring-textColor duration-300 transition-all absolute right-0 top-0 mt-24 mr-[372px] flex items-center rounded-sm shadow-sm shadow-black">
+    <div className="w-[307px] h-[213px] bg-containerColor overflow-y-auto hover:ring-2 hover:ring-textColor duration-300 transition-all absolute right-0 top-0 mt-24 mr-[372px] flex items-center rounded-sm shadow-sm shadow-black">
       <div className="font-inconsolata text-textColor text-left self-start text-[18px] ml-5 mt-2">
         <div className="text-[24px]">Work.</div>
         <HeroDetails heroName={name} fields={["occupation"]} />
